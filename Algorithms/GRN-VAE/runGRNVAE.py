28f1b5e1476ff7c64c9a2546fc738cfdd5ab54d5
ClaraRenou <56790973+ClaraRenou@users.noreply.github.com>
2023-05-25 11:48:16 +0200

    Update runGRNVAE.py

--- Algorithms/GRN-VAE/runGRNVAE.py
+++ Algorithms/GRN-VAE/runGRNVAE.py
@@ -1,1 +1,44 @@
-to be done
+import os
+import argparse
+import pandas as pd 
+import numpy as np
+import sys
+import scanpy as sc
+sys.path.append('grnvae') #to add a path to search for the requested module
+
+from grnvae.runner import runGRNVAE, DEFAULT_GRNVAE_CONFIGS
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description='Run scSGL algorithm.') 
+    parser.add_argument('--expression_file', 
+        help='Path to ExpressionData file')
+    parser.add_argument('--ref_net_file', 
+        help='Path to refNetwork file')
+    parser.add_argument('--out_file',
+        help='Path to output file')
+    
+    return parser
+
+def parse_arguments():
+    parser = get_parser()
+    opts = parser.parse_args()
+
+    return opts
+
+def main(args):
+
+    opts = parse_arguments()
+    expression_df = sc.read(f'opts.expression_file').transpose  #to read gene expression file
+    ref_net_df = pd.read_csv(opts.ref_net_file) #to read reference network file
+
+    #Learn signed graph with the parameters
+    vae, _ = runGRNVAE(
+    expression_df, DEFAULT_GRNVAE_CONFIGS, ground_truth=none)
+    A = vae.get_adj()
+    
+    A.to_csv(opts.out_file, index = False, sep = '\t')  #to write the output file
+
+if __name__ == "__main__":
+    main(sys.argv)
